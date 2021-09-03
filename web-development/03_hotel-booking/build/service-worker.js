3d0daaf38d31a6de08c30e73781af56424e27e2b
Ray Yam <ryamhkdev@gmail.com>
2021-09-04 00:14:18 +0800

    fix gh page relative path

--- web-development/03_hotel-booking/build/service-worker.js
+++ web-development/03_hotel-booking/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.85d4aa34ba9c68f8782cd3f909fc8d3c.js"
+  "https://rayyamhk.github.io/Self-learning/web-development/03_hotel-booking/build/precache-manifest.63b0ef807c4cea64ffc73e79da4fb1f2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://rayyamhk.github.io/Self-learning/web-development/03_hotel-booking/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
---
+++ web-development/05_pexels-clone/build/precache-manifest.6d42a808f0c545c5a6a5e75def36665c.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c4d0eab6f916891a2142372bbffa2c",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/index.html"
+  },
+  {
+    "revision": "cff11b788090b1927da5",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/css/main.648ab5aa.chunk.css"
+  },
+  {
+    "revision": "aea84a7e6b512fba5c57",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/js/2.0b234db7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/js/2.0b234db7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cff11b788090b1927da5",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/js/main.34fd3f48.chunk.js"
+  },
+  {
+    "revision": "7948490f3a901231a472",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/js/runtime-main.5d13ed39.js"
+  },
+  {
+    "revision": "6255bcbcf1d8433cdffd611272c7bcd4",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/01.6255bcbc.jpg"
+  },
+  {
+    "revision": "9078a0468a1ae5e103f359566221349b",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/01.9078a046.jpg"
+  },
+  {
+    "revision": "05d3c3bbfd6228130d97ff68f579c5ae",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/02.05d3c3bb.jpg"
+  },
+  {
+    "revision": "e5f257d1ed9b7d3b3637d9730cad9c21",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/02.e5f257d1.jpg"
+  },
+  {
+    "revision": "3e19966d8872a4f88abd8c2b468d090a",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/03.3e19966d.jpg"
+  },
+  {
+    "revision": "40239291fd9a90e5846a0eb84b10f903",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/03.40239291.jpg"
+  },
+  {
+    "revision": "8fa076badae0319d02d8dbfd3e78d3c0",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/04.8fa076ba.jpg"
+  },
+  {
+    "revision": "d71ac8dbec86963ab40633d9c5b7c9ca",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/04.d71ac8db.jpg"
+  },
+  {
+    "revision": "16f84d18e677e1406267e64f01daf143",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/05.16f84d18.jpg"
+  },
+  {
+    "revision": "201b41f4c405d9dcd75b45fedc273185",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/05.201b41f4.jpg"
+  },
+  {
+    "revision": "c6235d74046f4cd61a2baacf5d1c89b7",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/06.c6235d74.jpg"
+  },
+  {
+    "revision": "fdaab7317689d6caf614e0d3613f16fb",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/06.fdaab731.jpg"
+  },
+  {
+    "revision": "0512c14725c6ef39df117a122f49b635",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/07.0512c147.jpg"
+  },
+  {
+    "revision": "ca236c19b678e599f380ecdbf00a69ed",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/07.ca236c19.jpg"
+  },
+  {
+    "revision": "701b1bd9dc331bcbc6a53de4b73b9f7f",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/08.701b1bd9.jpg"
+  },
+  {
+    "revision": "d6f612b104ca019dd71dfff3035f43fc",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/08.d6f612b1.jpg"
+  },
+  {
+    "revision": "88151def98ad33a866435ad31e4179e7",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/09.88151def.jpg"
+  },
+  {
+    "revision": "a960d6d418a53c579b92888d4d621f8f",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/09.a960d6d4.jpg"
+  },
+  {
+    "revision": "73f792ed9c8eb2563b2da8bcfc465d1a",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/10.73f792ed.jpg"
+  },
+  {
+    "revision": "8f9bdabe83e70359fea4c819f2376652",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/10.8f9bdabe.jpg"
+  },
+  {
+    "revision": "ca6e5b02d4a7b3dcf4a9c4c78bd0e275",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/11.ca6e5b02.jpg"
+  },
+  {
+    "revision": "e1e61c21184b96b2fbdc1d672a39f2d2",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/11.e1e61c21.jpg"
+  },
+  {
+    "revision": "3aa38356d6f4156fd6b5c9f639257488",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/12.3aa38356.jpg"
+  },
+  {
+    "revision": "56f99cfee5a03a6d96b4d8d196487cf0",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/12.56f99cfe.jpg"
+  },
+  {
+    "revision": "37cb5c3a4f32696a54b9612a79ba4c6e",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/13.37cb5c3a.jpg"
+  },
+  {
+    "revision": "b5a893f5567e3585a0ebb1f5ad32174c",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/13.b5a893f5.jpg"
+  },
+  {
+    "revision": "443aa664825d9511ccfbdd6c9485adaf",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/14.443aa664.jpg"
+  },
+  {
+    "revision": "e4f4f3522e64fc130dfa8742ae795b4d",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/14.e4f4f352.jpg"
+  },
+  {
+    "revision": "75e62f1c7c36a0089ce4966a9e050298",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/15.75e62f1c.jpg"
+  },
+  {
+    "revision": "ccee5bcb0defc44013202f0b70487af9",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/15.ccee5bcb.jpg"
+  },
+  {
+    "revision": "5461c1ddec4eb5252d1a692cce413ae3",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/16.5461c1dd.jpg"
+  },
+  {
+    "revision": "71f7493a0a13a74721d557beebe535a8",
+    "url": "https://rayyamhk.github.io/Self-learning/web-development/05_pexels-clone/build/static/media/16.71f7493a.jpg"
+  }
+]);
